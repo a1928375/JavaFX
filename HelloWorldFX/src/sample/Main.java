c22612335e9b3d209d984c64a7505e623302d98e
Arthur Shih <a1928375@hotmail.com>
2020-04-29 11:42:22 +0800

    Add files via upload

---
+++ HelloWorldFX/src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Text text = new Text();
+        text.setFont(new Font(16));
+        text.setX(50);
+        text.setY(100);
+        text.setText("Hello World");
+
+        Group root = new Group(text);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
